119c03a65cd44511541eef4fd04322578279942d
Francisco Romero <francisco.romero@sjsu.edu>
2019-03-04 20:16:31 -0800

    Added a history page and a navbar cart counter
    History page contains a table of items from fakeHistoryService. The navbar also has a counter showing how many items are in the cart array. Also, fixed the price sorting bug.

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, {Component } from "react";
+import HistoryTable from './historyTable'
+import Pagination from '../common/pagination';
+import {getHistory, getHistoryItem} from '../services/fakeHistoryService';
+import {paginate} from '../utils/paginate';
+import { getAisles } from "../services/fakeAisleService";
+import _ from 'lodash';
+import { deleteProperty } from './../common/deleteProperty';
+import { getProduct } from './../services/fakeProductService';
+class History extends Component {
+    state = { 
+        products: [],
+        aisles: [],
+        currentPage: 1,
+        pageSize: 4,
+        sortColumn: {path: 'title', order: "asc"},
+    };
 
-const History = () => {
-    return <h1> This is the History page </h1>
-};
+
+    componentDidMount() {
+        const aisles = [{ _id: '', name: 'All Aisles'}, ...getAisles()];
+        var products = getHistory();
+        products.forEach( product => {
+            if(product.salePrice === 0) {
+                product.currentPrice = product.basePrice;
+                }
+            else{
+                product.currentPrice = product.salePrice;
+            }        
+        });
+        this.setState({products: getHistory(), aisles});
+    }
+
+    handleAddtoCart = (product) => {
+        let modifiedItem = deleteProperty(product, 'orderNum');
+        modifiedItem = deleteProperty(modifiedItem,'dateOfPurchase');
+        modifiedItem = {...modifiedItem, numberInStock: getProduct(product._id).numberInStock};
+        this.props.onAddToCart(modifiedItem);
+        
+    };
+
+    handlePriceChange = (product) => {
+        const salePrice = product.salePrice;
+        const basePrice = product.basePrice;
+        if(salePrice === 0) {
+        return <span> ${basePrice.toFixed(2)} </span>
+        }
+        return <span>${product.currentPrice.toFixed(2)}</span>
+    }
+
+    handlePageChange = page =>{
+        this.setState({currentPage: page});
+    }
+
+    handleAisleSelect = aisle => {
+        this.setState({selectedAisle: aisle, currentPage:1})
+    };
+
+    handleSort = sortColumn => {
+        this.setState({sortColumn});
+    };
+
+    getPagedData = () => {
+        const {pageSize, currentPage,sortColumn, selectedAisle, products: allProducts} = this.state;
+        const filtered = selectedAisle && selectedAisle._id
+        ? allProducts.filter(m => m.genre._id === selectedAisle._id) 
+        : allProducts;
+        
+        const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+        const products = paginate(sorted, currentPage, pageSize);
+        return {totalCount: filtered.length, data: products};
+    }
+
+    render(){
+        const { length: count} = this.state.products;
+        const {pageSize, currentPage,sortColumn} = this.state;
+        if(count ===0 ) return <p>There are no products in the database.</p>;
+
+        const {totalCount, data: products} = this.getPagedData();
+
+        return(
+            <div className="row">
+                <div className="col">
+                    <HistoryTable 
+                        products={products}
+                        sortColumn={sortColumn}
+                        setPrice = {this.handlePriceChange} 
+                        onAdd={this.handleAddtoCart}
+                        onSort={this.handleSort}
+                    />
+                    <Pagination 
+                        itemsCount={totalCount} 
+                        pageSize = {pageSize} 
+                        currentPage={currentPage}
+                        onPageChange={this.handlePageChange}
+                    />
+                </div>
+            </div>
+           
+        );   
+    }
+}
+ 
 
 export default History;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,36 +1,47 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {Link, NavLink} from "react-router-dom";
 import Logo from "../images/ShoppingCart.png";
-const NavBar = () =>
-{
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand" to="/">
-  <img className="logo" src={Logo} width="30" height="30" alt="Logo"/>
-  Grocer-E
-  </Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-    <li className="nav-item">
-        <NavLink className="nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
-      </li>
+
+class NavBar extends Component
+{
+  numberFormat = (number) =>
+  {
+    if(number === 0)
+    {
+      return "";
+    }
+    return <span> ({number}) </span>;
+  }
+  render(){
+      return (
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <Link className="navbar-brand" to="/">
+    <img className="logo" src={Logo} width="30" height="30" alt="Logo"/>
+    Grocer-E
+    </Link>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <NavLink className="nav-link" to="/aisles">Aisles <span className="sr-only">(current)</span></NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/history">History</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" to="/cart">Cart</NavLink>
-      </li>
-     
-    </ul>
-  </div>
-</nav>
-    );
+          <NavLink className="nav-link" to="/home">Home <span className="sr-only">(current)</span></NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/aisles">Aisles <span className="sr-only">(current)</span></NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/history">History </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/cart">Cart{this.numberFormat(this.props.cart.length)} </NavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+      );
+  };
 }
 export default NavBar;
