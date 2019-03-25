c5a98397ea54689eb133c8a4e04f213b72e6ebab
Francisco Romero <francisco.romero@sjsu.edu>
2019-03-25 10:34:17 -0700

    Auto stash before merge of "master" and "origin/master"

--- src/components/historyTable.jsx
+++ src/components/historyTable.jsx
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Table from '../common/table';
 
 class HistoryTable extends Component {
     columns = [
-        {path: 'productName', label: 'Product'},
+        {path: 'productName', label: 'Product', content: product =>
+         <Link to={`/aisles/${product._id}`}>{product.productName}</Link>
+        },
         {path: 'aisle.name', label: 'Aisle'},
         {path: 'currentPrice', label: 'Price', content: product => (
             this.props.setPrice(product)
