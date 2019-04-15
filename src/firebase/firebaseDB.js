fb1b548c3c4b916a72ece9d4e96e3c8eb4273e0a
Nick Castro <3ncastro@gmail.com>
2019-04-15 11:12:28 -0700

    added save and get cart

--- src/firebase/firebaseDB.js
+++ src/firebase/firebaseDB.js
@@ -1,3 +1,38 @@
 import { fire } from "./firebase";
+import React, { Component } from "react";
 
-// Write functions here. Use "fire.database()..." to access DB
+class firebaseDB {
+  state = {
+    cart: []
+  };
+
+  getCart() {
+    var items = [];
+    var users = "UniqueID";
+    const moviesRef = fire
+      .database()
+      .ref(users + "/cart")
+      .orderByKey();
+    moviesRef
+      .once("value", snap => {
+        snap.forEach(child => {
+          this.setState({ cart: items });
+          items.push(child.val());
+        });
+      })
+      .then(() => {
+        console.log(items);
+        this.setState({ cart: items });
+      });
+  }
+
+  saveCart() {
+    var cartToSave = ["this", "is", "a", "test"]; //someone need to give this
+    var users = "UniqueID"; //someone need to give this
+    var ref = fire;
+    var moviesRef = ref.database().ref(users + "/cart");
+    moviesRef.set(cartToSave);
+  }
+}
+
+export default firebaseDB;
