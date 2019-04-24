48aae184c46c40ebb2ae80c32350bcb620bf4747
Francisco Romero <francisco.romero@sjsu.edu>
2019-04-24 11:16:23 -0700

    Added Jquery for collapsed navbar navigation
    Installed Jquery and popper.js in order for bootstrap components to use js functions. Collapsed Navbar now properly displays menu options.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'; 
