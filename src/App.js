bdb639b41e4df7033ad55636f7be3f78c0259747
Kevin Ma <vetagkevin@gmail.com>
2019-04-25 13:36:12 -0700

    Passed down clearCart function to shoppingCart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Products from "./components/products";
 import History from "./components/history";
 import ProductDescription from "./components/productDescription";
 import ShoppingCart from "./components/shoppingCart";
+import Checkout from "./components/checkoutPage";
 import HomePage from "./components/homePage";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
@@ -44,11 +45,6 @@ class App extends Component {
   setCart = cart => {
     this.setState({ cart });
   };
-
-  // handleDelete = product => {
-  //   const cartTemp = this.state.cart.filter(c => c.id !== product);
-  //   this.setState({ cart: cartTemp });
-  // };
 
   handleDelete = item => {
     let cartTemp = this.state.cart;
@@ -177,6 +173,7 @@ class App extends Component {
                   onIncrement={this.addToQuantity}
                   onDecrement={this.subtractFromQuantity}
                   onDelete={this.handleDelete}
+                  clearCart={this.clearCart}
                 />
               )}
             />
