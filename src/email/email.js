84a28a54e564681358c635ef0232da3477e84e04
Dominic Pham <dmncpham@Dominics-MacBook-Air.local>
2019-04-28 00:11:30 -0700

    Update parameters for emailJS

--- src/email/email.js
+++ src/email/email.js
@@ -1,10 +1,10 @@
 import * as emailjs from 'emailjs-com'
-export function sendEmail(message){
+export function sendEmail(receiverAddress, subject, message){
     emailjs.init("user_QIOTKQgxx593KdLcNW2Sj");
     var templateParams = {
-        text: 'James',
-        user_name: 'Check this out!',
-        user_email: 'MarkeyMarke133@gmail.com'
+        user_email: receiverAddress,
+        subject: subject,
+        message: message
     };
      
     emailjs.send('default_service', 'contact_form', templateParams)
