56e4f2900d50e34ce58a199ad7a46fdd14f88fb7
Mark <casapao.markkenneth@gmail.com>
2019-04-28 19:17:59 -0700

    Styled login buttons

--- src/components/loginButton.jsx
+++ src/components/loginButton.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const LoginButton = props => {
   return (
-    <button onClick={props.onClick} type="button" className="btn btn-primary">
+    <button
+      onClick={props.onClick}
+      type="button"
+      className="btn btn-danger btn-lg"
+    >
       Login
     </button>
   );
