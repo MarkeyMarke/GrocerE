01722724dc23a273bce8ca3d7a0d994f3ce9e1d1
Dominic Pham <dmncpham@Dominics-MacBook-Air.local>
2019-04-29 11:30:47 -0700

    Remove testing code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import PasswordRecovery from "./components/passwordRecovery";
 import firebase from "firebase";
 import { getUID } from "./firebase/firebaseAuth.js";
 import { getCart } from "./firebase/firebaseDB.js";
-import { sendEmail } from "./email/email.js";
 import LoadingOverlay from "react-loading-overlay";
 import FadeLoader from "react-spinners/FadeLoader";
 import Footer from "./common/footer";
@@ -108,8 +107,6 @@ class App extends Component {
         }));
       }
     });
-    var itemArray = ["Test", "fdsaf", "fdsa"];
-    sendEmail("MarkeyMarke133@gmail.com", "Test", 10, itemArray, "fdsa", 10);
   }
 
   render() {
